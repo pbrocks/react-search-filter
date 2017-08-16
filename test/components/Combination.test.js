653b3711ee68bd7bd6c4819cee4539b15162ff04
danielmoi <daniel@ampersandmoi.com>
2017-08-16 21:30:15 +1000

    Add test

--- test/components/Combination.test.js
+++ test/components/Combination.test.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { fromJS } from 'immutable';
+import { stub } from 'sinon';
 
 import Combination from '../../src/components/Combination';
 import H from '../helpers';
 import { combination, list } from '../fixtures';
 
 
-describe('<CombinationComponent />', () => {
-  it.only('renders', () => {
+describe.only('<Combination />', () => {
+  it('renders', () => {
     const wrapper = shallow(
       <Combination
         index={0}
@@ -19,5 +20,60 @@ describe('<CombinationComponent />', () => {
       />,
     );
     expect(wrapper.find('.rsf__combination-container').length).toEqual(1);
+  });
+
+  it('handles deleting a combination', () => {
+    const deleteStub = stub();
+    const wrapper = shallow(
+      <Combination
+        index={0}
+        combination={fromJS(combination)}
+        list={fromJS(list)}
+        saveCombination={H.VOID}
+        deleteCombination={deleteStub}
+      />,
+    );
+    wrapper.find('.rsf__combination-delete').simulate('click');
+    expect(deleteStub.callCount).toEqual(1);
+  });
+
+  it('handles clicking on combination search', () => {
+    const wrapper = shallow(
+      <Combination
+        index={0}
+        combination={fromJS(combination)}
+        list={fromJS(list)}
+        saveCombination={H.VOID}
+        deleteCombination={H.VOID}
+      />,
+    );
+    expect(wrapper.state('isEditing')).toEqual(false);
+    wrapper.find('.rsf__combination-search').simulate('click');
+    expect(wrapper.state('isEditing')).toEqual(true);
+    console.log('wrapper.debug():', wrapper.debug());
+
+    const input = wrapper.find('.rsf__search-input');
+    expect(wrapper.state('search')).toEqual('Lugia');
+    input.simulate('change', { target: { value: 'Cookie Monster' } });
+    expect(wrapper.state('search')).toEqual('Cookie Monster');
+
+    input.simulate('keyDown', { which: 40 });
+    expect(wrapper.state('isListVisible')).toEqual(true);
+  });
+
+  it('handles clicking on combination filter', () => {
+    const wrapper = shallow(
+      <Combination
+        index={0}
+        combination={fromJS(combination)}
+        list={fromJS(list)}
+        saveCombination={H.VOID}
+        deleteCombination={H.VOID}
+      />,
+    );
+    expect(wrapper.state('isListVisible')).toEqual(false);
+
+    wrapper.find('.rsf__combination-filter').simulate('click');
+    expect(wrapper.state('isListVisible')).toEqual(true);
   });
 });
